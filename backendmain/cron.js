aebbd00af4c0f5982d2ee180ed376dfb212d2686
aagammodi <modiaagam10@gmail.com>
2024-04-01 23:28:12 +0530

    [Updated]: cron updated

--- backendmain/cron.js
+++ backendmain/cron.js
@@ -6,34 +6,34 @@ import schedule from "node-schedule";
 
 // second minute hour day month day_of_week
 const job = schedule.scheduleJob("59 * * * * *", async () => {
-  let currRequests = await currReqModel.find({});
-  if (currRequests != null) {
-    currRequests.forEach((curr) => {
-      Notreplied(curr.Referral_ID);
-    });
-  }
+  // let currRequests = await currReqModel.find({});
+  // if (currRequests != null) {
+  //   currRequests.forEach((curr) => {
+  //     Notreplied(curr.Referral_ID);
+  //   });
+  // }
 });
 
 const jobDaily = schedule.scheduleJob("1 1 0 * * *", async () => {
-  let dateToCheck = (getDate()-2);
-  let currRequests = await currReqModel.find({Latest_Req_Date: {$lte: dateToCheck}});
-  if (currRequests != null) {
-    currRequests.forEach((curr) => {
-      Notreplied(curr.Referral_ID);
-    });
-  }
+  // let dateToCheck = (getDate()-2);
+  // let currRequests = await currReqModel.find({Latest_Req_Date: {$lte: dateToCheck}});
+  // if (currRequests != null) {
+  //   currRequests.forEach((curr) => {
+  //     Notreplied(curr.Referral_ID);
+  //   });
+  // }
 });
 
 const jobMonthly = schedule.scheduleJob("0 0 0 1 * *", async () => {
-  await proreferusers.updateMany(
-    { Company_ID: { $ne: null } },
-    {
-      $set: {
-        Referrals_Reviewed_ThisMonth: 0,
-        Referrals_Requested_ThisMonth: 0,
-      },
-    }
-  );
+  // await proreferusers.updateMany(
+  //   { Company_ID: { $ne: null } },
+  //   {
+  //     $set: {
+  //       Referrals_Reviewed_ThisMonth: 0,
+  //       Referrals_Requested_ThisMonth: 0,
+  //     },
+  //   }
+  // );
 });
 
 // const jobDemo = schedule.scheduleJob("0 18 13 * * *", async () => {
