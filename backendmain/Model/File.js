3a01a65ad6a23c75f987a83036a4adba73dbd46a
aagammodi <modiaagam10@gmail.com>
2024-04-01 17:28:29 +0530

    [Deleted]: backend and frontend deleted

--- backendmain/Model/File.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { mongo } from 'mongoose';
-
-const fileSchema = new mongoose.Schema({
-    User_ID: Number,
-    data: Buffer,
-});
-
-const File = mongoose.model('Resume', fileSchema);
-export default File;
